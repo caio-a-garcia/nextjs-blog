ef1e81c7ae0350241e8fe52d387b8d96766e2e2b
Caio Garcia <82896956+caio-a-garcia@users.noreply.github.com>
2023-10-19 19:47:43 -0300

    Test change profile image

--- components/layout.js
+++ components/layout.js
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
 	  <>
 	    <Image
 	      priority
-	      src="/images/Perfil2023.jpeg"
+	      src="/images/profile.jpg"
 	      className={utilStyles.borderCircle}
 	      height={144}
 	      width={144}
