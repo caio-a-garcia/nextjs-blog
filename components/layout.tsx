87a1030d19cfce155fd662bf68c194b713c210d2
Caio Garcia <caio.augusto@algar.com.br>
2023-10-22 18:35:08 -0300

    Change blog name and specify Layout argument types

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,10 +5,16 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Caio Garcia'
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Blog do Caio';
 
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
