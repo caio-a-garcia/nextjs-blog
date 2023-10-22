1615a39bebf59a77fb5b4e478321f049efe7a9d0
Caio Garcia <caio.augusto@algar.com.br>
2023-10-22 18:29:38 -0300

    Fix [id] regarding using TS

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -6,7 +6,15 @@ import utilStyles from '../../styles/utils.module.css';
 import { GetStaticProps, GetStaticPaths } from 'next';
 
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
@@ -21,8 +29,8 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
   return {
     props: {
       postData,
@@ -30,7 +38,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
